6bb2173bf3191b6ebee9f5caff72ccf77a501c96
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-11-25 01:11:06 -0300

    estilandoDetalle

--- src/app/components/tarjeta-pokemon/tarjeta-pokemon.component.ts
+++ src/app/components/tarjeta-pokemon/tarjeta-pokemon.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 import { Resultado } from 'src/app/interfaces/pokeapi';
 import { PokemonService } from '../../services/pokemon.service';
+import { Pokemon } from 'src/app/interfaces/pokemon';
 
 @Component({
   selector: 'app-tarjeta-pokemon',
@@ -19,13 +20,22 @@ export class TarjetaPokemonComponent implements OnChanges{
 
   @Input() data?: Resultado;
   @Input() seleccionado: boolean = false;
+  @Input() fullData?: Pokemon;
   @Output() clickeado = new EventEmitter<string>()
   id: string = '0';
 
   extraerInformacion(){
-    if(this.data){
+    if(this.data && this.data.url !== ''){
       this.id = this.data.url.substring(34, this.data.url.length-1);
       this.pokemonService.getById(this.id);
+      return
+    }
+    if(this.fullData){
+      this.id = this.fullData.species.url.substring(42, this.fullData.species.url.length-1);
+      this.data = {
+        name: this.fullData.species.name,
+        url: ''
+      }
     }
   }
 }
